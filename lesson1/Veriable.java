ce0864cea8b1fdf84182dc9ad67797072645b5cf
Igor <Cherepkovie@gmail.com>
2020-07-08 14:43:12 +0300

    implement HW1

---
+++ lesson1/Veriable.java
@@ -0,0 +1,6 @@
+ public class Veriable  {
+ 	public static void main(String[] args) {
+  //		System.out.println(" class Veriable was created. Huraaaaaaaah ! ");
+ 		System.out.println("Программа Veriable создана. Уррраааа!");
+ 	}
+ }
