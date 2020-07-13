2787c241783ec115432ba698e4588bf3938bb001
Igor <Cherepkovie@gmail.com>
2020-07-13 16:25:16 +0300

    formated

--- lesson1/ConditionalStatement.java
+++ lesson1/ConditionalStatement.java
@@ -1,24 +1,31 @@
 public class ConditionalStatement {	
     public static void main(String[] args) {
 //        System.out.println("Hello, world");
-    	int age = 21;
-    	char maleGender = '2';
-    	float height = 1.6F;
-    	String firstWord = "K";
+        int age = 21;
+        char maleGender = 'w';   /* половая принадлежность 'm' или 'w'*/
+        float height = 1.6F;
+        String firstNameLetter = "K";
 
         System.out.println(" ");
-        if (age > 20) {System.out.println("Relevant age category");}
+        if (age > 20) {
+            System.out.println("Relevant age category");}
 
-    	if (maleGender != 'm') { 
-    	System.out.println("We need in man.You are not satisfied our conditions");}
-       	if (maleGender == 'm') {System.out.println("WELCOME");}
+        if (maleGender != 'm') { 
+            System.out.println("We need in man.You are not satisfied our conditions");}
+        if (maleGender == 'm') {
+            System.out.println("WELCOME");}
 
-    	if (height < 1.8) {System.out.println("Too smal guy  " + height);}
-    	else {System.out.println("OK  " + height);}
+        if (height < 1.8) {
+            System.out.println("Too smal guy  " + height);}
+        else {
+            System.out.println("OK  " + height);}
 
-    	if (firstWord == "M") {System.out.println("Really M");}
-    	else if (firstWord == "I") {System.out.println("Really I");}
-    	else {System.out.println("It is  not be really M or I  it is   " + firstWord);} 
-    	System.out.println(" "); 
+        if (firstNameLetter == "M") {
+            System.out.println("Really M");}
+        else if (firstNameLetter == "I") {
+            System.out.println("Really I");}
+        else {
+            System.out.println("It is  not be really M or I  it is   " + firstNameLetter);} 
+        System.out.println(" "); 
     }
 }
