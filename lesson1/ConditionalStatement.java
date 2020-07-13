b57c1d06023e3fd5ee44afb222d3d025a2f9cc51
Igor <Cherepkovie@gmail.com>
2020-07-14 00:34:31 +0300

    format minor fixes

--- lesson1/ConditionalStatement.java
+++ lesson1/ConditionalStatement.java
@@ -1,31 +1,35 @@
-  public class ConditionalStatement {	
+public class ConditionalStatement {	
     public static void main(String[] args) {
-//        System.out.println("Hello, world");
+        //System.out.println("Hello, world");
         int age = 21;
-        char maleGender = 'w';   /* половая принадлежность 'm' или 'w'*/
-        float height = 1.6F;
-        String firstNameLetter = "K";
-
         System.out.println(" ");
         if (age > 20) {
-            System.out.println("Relevant age category");}
+            System.out.println("Relevant age category");
+        }
 
+        char maleGender = 'w';   /* половая принадлежность 'm' или 'w'*/
         if (maleGender != 'm') { 
-            System.out.println("We need in man.You are not satisfied our conditions");}
+            System.out.println("We need in man.You are not satisfied our conditions");
+        }
         if (maleGender == 'm') {
-            System.out.println("WELCOME");}
+            System.out.println("WELCOME");
+        }
 
+        float height = 1.6F;
         if (height < 1.8) {
-            System.out.println("Too smal guy  " + height);}
-        else {
-            System.out.println("OK  " + height);}
+            System.out.println("Too smal guy  " + height);
+        } else {
+            System.out.println("OK  " + height);
+        }
 
+        String firstNameLetter = "K";
         if (firstNameLetter == "M") {
-            System.out.println("Really M");}
-        else if (firstNameLetter == "I") {
-            System.out.println("Really I");}
-        else {
-            System.out.println("It is  not be really M or I  it is   " + firstNameLetter);} 
+            System.out.println("Really M");
+        } else if (firstNameLetter == "I") {
+            System.out.println("Really I");
+        } else {
+            System.out.println("It is  not be really M or I  it is   " + firstNameLetter);
+        } 
         System.out.println(" "); 
     }
 }
--- lesson1/Cycle.java
+++ lesson1/Cycle.java
@@ -1,25 +1,26 @@
-  public class Cycle {	
+public class Cycle {	
     public static void main(String[] args) {
         // Печать цифр от 1 до 20 c for         
         for (int i = 0; i <= 20; i++) {
-           System.out.println(i);     }
-           System.out.println(" ");
+           System.out.println(i);     
+        }
+        System.out.println(" ");
 
         //  Печать цифр от 6 до -6 с while
-        int sh = 6;
-        while (sh >= -6) {
-            System.out.println("ordinary step  " + sh);
-            sh -= 2;     }
+        int stepIterac = 6;
+        while (stepIterac >= -6) {
+            System.out.println("ordinary step  " + stepIterac);
+            stepIterac -= 2;     }
 
         // Cумма нечетных чисел от 10 до 20 с do  while
-        int j = 10; int result = 0;
+        int j = 10;
+        int result = 0;
         do { 
             j++;
-            if (j%2 != 0 )
+            if (j % 2 != 0 )
                 result += j;  
-           } 
-        while(j < 20);
+           } while(j < 20);
         System.out.println("        Sublimation        ");
-        System.out.println("SUMMRY = " + result);
+        System.out.println("SUM = " + result);
     }
 }   
