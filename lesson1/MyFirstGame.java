0d44e2714bf717d9a9dea9ae74aa0990ad0b84d1
Igor <Cherepkovie@gmail.com>
2020-07-20 20:15:12 +0300

    the last corrections 20/07/20

--- lesson1/MyFirstGame.java
+++ lesson1/MyFirstGame.java
@@ -1,32 +1,28 @@
 public class MyFirstGame {	
     public static void main(String[] args) {
         // компьютер "загадывает" целое число от (0 до 100], которое вам необходимо угадать
-        int playerNum = 90;
-        int hiddenNum = 85;
+        int playerNum = 2;
+        int hiddenNum = 1;
         int left = 0;
         int right = 100;
         int deltaNum = playerNum-hiddenNum;
-        System.out.println(" ");
-        System.out.println("Input number between  " + left + "  and  " + right);
-        System.out.println(" ");
+
+        System.out.println("\nInput number between  " + left + "  and  " + right + "\n");
         while (playerNum != hiddenNum) {
-            System.out.println("Your number is   " + playerNum + "   You didn't guess");
-            System.out.println(" ");
-            if (playerNum > left && hiddenNum >= playerNum) {
+            System.out.println("Your number is   " + playerNum + "   You didn't guess\n");
+            if (playerNum > left && hiddenNum > playerNum) {
                 left = playerNum;
-            } else if (playerNum >= hiddenNum && right > hiddenNum) {
+            } else if (playerNum > hiddenNum && right > hiddenNum) {
                 right = playerNum; 
             }
             deltaNum = playerNum-hiddenNum;
-                if (deltaNum >0) {
-                    playerNum = playerNum-1;
-                }
-                else if (deltaNum<0) {
-                    playerNum = playerNum+1;
-                }
-                System.out.println("Input number between  " + left + "  and  " + right);
+            if (deltaNum > 0) {
+                playerNum--;
+            } else if (deltaNum < 0) {
+                playerNum ++;
+            }
+            System.out.println("Input number between  " + left + "  and  " + right);
         }
-        System.out.println("You are right   " + hiddenNum);
-        System.out.println(" ");
+        System.out.println("You are right   " + hiddenNum + "\n");
     }
 }   
--- lesson1/Unicode.java
+++ lesson1/Unicode.java
@@ -2,12 +2,9 @@ public class Unicode {
     public static void main(String[] args) {
         // Выведите в цикле символы кодировки Unicode в диапазоне [9398, 10178]. 
         // Если выводятся кракозябры, то [33, 126]
-        char i = 33;
         int j = 33;
-        for (i = 33; i <= 126; i++) {
-            System.out.print(j);
-            System.out.print("  ");
-            System.out.println(i);
+        for (char i = 33; i <= 126; i++) {
+            System.out.println(j + "  " + i);
             j++;
         }
     }
