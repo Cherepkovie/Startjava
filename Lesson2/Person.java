fd0e943b6a971c962799e4fdef27d15661c181de
Igor <Cherepkovie@gmail.com>
2020-07-23 15:50:23 +0300

    Without main the first 23/07/20

---
+++ Lesson2/Person.java
@@ -0,0 +1,35 @@
+public class Person {
+
+    String gender = "Man";
+    String name = "name";
+    float height = 1.81F;
+    float weight = 92,8F;
+    int age = 58;
+    int speed = 5;
+    highspeed = 10;
+
+    int go() {
+    System.out.println("Moving");
+    return speed;
+    }
+
+    void sit() {
+    System.out.println("Rest");
+    }
+
+    int run() {
+    System.out.println("High speed moving");
+    return highspeed;
+    }
+
+    boolean speak() {
+    System.out.println("Speak English");
+    return true;
+    }
+
+    string learning() {
+    System.out.println("java learning");
+    return "online";
+    }
+
+}
