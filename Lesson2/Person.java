c23bd00bd1f543e99eaae03d6ae8854788baf44d
Igor <Cherepkovie@gmail.com>
2020-07-26 18:51:46 +0300

    correction a little bit 26/07/20

--- Lesson2/Person.java
+++ Lesson2/Person.java
@@ -1,35 +1,34 @@
 public class Person {
 
-    String gender = "Man";
-    String name = "name";
-    float height = 1.81F;
-    float weight = 92,8F;
-    int age = 58;
-    int speed = 5;
-    highspeed = 10;
+        String gender = "Man";
+        String name = "Igor";
+        float height = 1.81F;
+        float weight = 92,8F;
+        int age = 58;
+        int speed = 5;
+        highspeed = 10;
 
-    int go() {
-    System.out.println("Moving");
-    return speed;
-    }
+        int go() {
+        System.out.println("Moving");
+        return speed;
+        }
 
-    void sit() {
-    System.out.println("Rest");
-    }
+        void sit() {
+        System.out.println("Rest");
+        }
 
-    int run() {
-    System.out.println("High speed moving");
-    return highspeed;
-    }
+        int run() {
+        System.out.println("High speed moving");
+        return highspeed;
+        }
 
-    boolean speak() {
-    System.out.println("Speak English");
-    return true;
-    }
+        boolean speak() {
+        System.out.println("Speak English");
+        return true;
+        }
 
-    string learning() {
-    System.out.println("java learning");
-    return "online";
-    }
-
+        string learning() {
+        System.out.println("java learning");
+        return "online";
+        }
 }
