aca5306ab8eb55ffaf211139611b675d0325ba47
Igor <Cherepkovie@gmail.com>
2020-07-27 16:57:05 +0300

    assign values

--- Lesson2/WolfTest.java
+++ Lesson2/WolfTest.java
@@ -1,12 +1,17 @@
 public class WolfTest {	
     public static void main(String[] args) {
         Wolf wolfOne = new Wolf();
+        wolfOne.name = "Limba";
+        wolfOne.gender = "Bitch";
+        wolfOne.colour = "Brown";
+        wolfOne.age = 1;
         System.out.println("\ndog " + wolfOne.gender);
         System.out.println("named  " + wolfOne.name);
         System.out.println("with durty colour " + wolfOne.colour);
         System.out.println("with age of  " + wolfOne.age  + "\n\n");
         wolfOne.go(); 
-       System.out.println(wolfOne.howl() + "\n"); 
-       System.out.println(wolfOne.hunting() + "\n\n\n\n\n");
+        System.out.println(wolfOne.howl() + "\n"); 
+        System.out.println(wolfOne.hunting() + "\n\n\n\n\n");
+        
     }
 }
