d286aa9a8286f6019cd404c9d32cc4ec4544cbfd
Igor <Cherepkovie@gmail.com>
2020-07-28 22:09:04 +0300

    get/set - correction

--- Lesson2/Wolf.java
+++ Lesson2/Wolf.java
@@ -1,33 +1,65 @@
 public class Wolf {
 
-    String gender = "male";
-    String name = "Wolfy";
-    float weight = 25.5F;
-    int age = 5;
-    String colour = "grey";
-    int speed;
-    int highspeed;
+    private String gender = "male";
+    private String name = "Wolfy";
+    private float weight = 25.5F;
+    private int age = 5;
+    private String colour = "grey";
+    private int speed;
+    private int highspeed;
 
-    int go() {
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        if (age > 8) {
+            System.out.println("\nAge is not correct\n");
+        } else {
+            this.age = age;
+          }
+    }
+
+    public int go() {
         System.out.println("Moving \n" );
         return speed;
     }
 
-    void sit() {
+    public void sit() {
         System.out.println("sit");
     }
 
-    int run() {
+    public int run() {
         System.out.println("High speed moving");
         return highspeed;
     }
 
-    boolean howl() {
+    public boolean howl() {
         System.out.println("Instill fear");
         return true;
     }
 
-    String hunting() {
+    public String hunting() {
         System.out.println("maintenance of life");
         return "endurance";
     }
--- Lesson2/WolfTest.java
+++ Lesson2/WolfTest.java
@@ -1,14 +1,21 @@
 public class WolfTest {	
     public static void main(String[] args) {
         Wolf wolfOne = new Wolf();
-        wolfOne.name = "Limba";
-        wolfOne.gender = "Bitch";
-        wolfOne.colour = "Brown";
-        wolfOne.age = 1;
-        System.out.println("\ndog " + wolfOne.gender);
-        System.out.println("named  " + wolfOne.name);
-        System.out.println("with durty colour " + wolfOne.colour);
-        System.out.println("with age of  " + wolfOne.age  + "\n\n");
+
+        System.out.println("\ndog " + wolfOne.getGender());
+        System.out.println("named  " + wolfOne.getName());
+        System.out.println("with durty colour " + wolfOne.getColour());
+        System.out.println("with age of  " + wolfOne.getAge() + " years old\n\n");
+
+        wolfOne.setGender("Bitch");
+        wolfOne.setName("Limba");
+        wolfOne.setColour("Brown");
+        wolfOne.setAge(10);
+        System.out.println("\ndog " + wolfOne.getGender());
+        System.out.println("named  " + wolfOne.getName());
+        System.out.println("with durty colour " + wolfOne.getColour());
+        System.out.println("with age of  " + wolfOne.getAge() + " years old\n\n");
+
         wolfOne.go(); 
         System.out.println(wolfOne.howl() + "\n"); 
         System.out.println(wolfOne.hunting() + "\n\n\n\n\n");
