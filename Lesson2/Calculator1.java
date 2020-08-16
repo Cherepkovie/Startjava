75d0dc5ce6fb1c975738112f6afede573ac6288f
Igor <Cherepkovie@gmail.com>
2020-08-16 17:19:34 +0300

    comments off

--- Lesson2/Calculator1.java
+++ Lesson2/Calculator1.java
@@ -1,12 +1,10 @@
 public class Calculator1 {	
-//    public static void main(String[] args) {
+
         private int number1 = 25;
         private int number2 = 3;
         private long result = 0L;
         private char sign = '^';
-//        System.out.println("empty");
 
-        
         public int getNumber1() {
         return number1;
         } 
@@ -27,24 +25,9 @@ public class Calculator1 {
         return sign;
         }
 
-    public void setSign(char sign) {
+        public void setSign(char sign) {
         this.sign = sign;
-    }
-
-/*     int month = 8;
-        String monthString;
-/*        switch (month) {
-            case 1:  monthString = "January";
-                     break;
-            case 2:  monthString = "February";
-                     break;
-            case 3:  monthString = "March";
-                     break;
-
-
-            if (sign == '+') {
-            result = number1 + number2;
-            System.out.println("Addition " + number1 + " and " + number2 + " = " + result);*/
+        }
 
             switch (sign) {
             case '+': result = number1 + number2;
@@ -71,30 +54,4 @@ public class Calculator1 {
                 System.out.println("Vozvesti " + number1 + " v stepen " + number2 + " = " + result);
             break;
             }
-    }        
-
-
-/*        } else if (sign == '-') {
-            result = number1 - number2;
-            System.out.println("Subtraction " + number1 + " and " + number2 + " = " + result);
-        } else if (sign == '/') 
-            result = number1 / number2;
-            System.out.println("Division " + number1 + " and " + number2 + " = " + result);
-        } else if (sign == '*') {
-            result = number1 * number2;
-            System.out.println("Multiplication " + number1 + " and " + number2 + " = " + result);
-        } else if (sign == '^') {
-            int i = 1;
-            result = 1;
-            do {
-                i++;
-                result *= number1;
-            } while (i <= number2);
-            System.out.println("Vozvesti " + number1 + " v stepen " + number2 + " = " + result);
-        } else if (sign == '%') {
-            result = number1 % number2;
-            System.out.println("Delenije po modulu  " + number1 + " and " + number2 + " = " + result);
-        } */
-
-//        System.out.println("  ");
-
+    }       
